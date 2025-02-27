ce6b2ca95fab5d71d7e89fd2e783cc17c2d819ec
Roryrex <rorywildy@gmail.com>
2025-02-27 13:13:30 +1300

    Setting Up Player
    WIP set up of player. Folders will be added and organized as the project progresses. (Github won't let me push origin just folders.)

---
+++ game_level_3/scripts/player.gd
@@ -0,0 +1,20 @@
+############################## Player ##############################
+extends CharacterBody2D
+class_name Player
+
+#Export Variables
+@export var MAX_SPEED : float
+
+#Other Variables
+var speed : float
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+func _ready():
+	pass
+
+
+#THE MAIN PHYSICS PROCESS
+#---------------------------------------------------------------------------------------------------------------------------
+func _physics_process(_delta):
+	pass
