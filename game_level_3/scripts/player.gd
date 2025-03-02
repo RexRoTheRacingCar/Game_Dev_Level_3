890e97e72afe971dd4ef38bb3a1942b925a875c1
Roryrex <rorywildy@gmail.com>
2025-03-02 19:41:35 +1300

    Dashing & Textures
    Added a basic play dash.
    Fixed anti-aliasing on tile map textures.

--- game_level_3/scripts/player.gd
+++ game_level_3/scripts/player.gd
@@ -2,15 +2,27 @@
 extends CharacterBody2D
 class_name Player
 
+
+#Node variables
+@export_group("Nodes")
+@export var p_dash_timer : Timer
+
 #Movement variables
 @export_group("Customisable")
 @export var p_speed : float = 425
-const P_ACCEL : int = 3000
-const P_FRICTION : int = 20000
+@export var P_ACCEL : int = 3000
+@export var P_FRICTION : int = 20000
 var p_vel_prep : Vector2
+var p_dashing : bool = false
+
+#Misc variables
+@export var p_dash_delay : float = 0.9
+
 
 #---------------------------------------------------------------------------------------------------------------------------
 func _ready():
+	#Variable prep
+	p_dashing = false
 	p_vel_prep = Vector2.ZERO
 	velocity = Vector2.ZERO
 
@@ -18,10 +30,16 @@ func _ready():
 #THE MAIN PHYSICS PROCESS
 #---------------------------------------------------------------------------------------------------------------------------
 func _physics_process(delta):
-	#Player Movement
+	#Player movement
 	var p_input = Input.get_vector("left", "right", "up", "down") #Get movement vector
 	player_movement(p_input, delta)
 	
+	#Player roll
+	if Input.is_action_just_pressed("dash"):
+		player_dash(p_input)
+	
+	#Apply motion
+	velocity = Vector2(p_vel_prep.x, p_vel_prep.y / 2)
 	var _error = move_and_slide()
 
 
@@ -36,5 +54,24 @@ func player_movement(p_input, delta):
 	else: 
 		p_vel_prep = p_vel_prep.move_toward(Vector2(0,0), delta * P_FRICTION)
 		
-	velocity = Vector2(p_vel_prep.x, p_vel_prep.y / 2)
 	
+
+
+#Player Dash Function
+#---------------------------------------------------------------------------------------------------------------------------
+func player_dash(p_input):
+	if p_dashing == false and p_input:
+		p_dashing = true
+		
+		p_vel_prep = p_input * 2200
+		
+		p_dash_timer.start(p_dash_delay)
+
+
+#------------------------------SIGNAL FUNCTIONS------------------------------
+
+#Player Dash Timer
+#---------------------------------------------------------------------------------------------------------------------------
+func _on_dash_timer_timeout():
+	if p_dashing == true:
+		p_dashing = false
