1a271dfe8ad62e5c6cf587179490f32bf6edce8c
Roryrex <rorywildy@gmail.com>
2025-03-13 13:17:06 +1300

    Fixing Hurtboxes and Piercing
    Fixed hurtboxes and bullet piercing, but there are some future issues that could still arise. Will do a rework of it soon.

--- game_level_3/scripts/components/hurtbox_component.gd
+++ game_level_3/scripts/components/hurtbox_component.gd
@@ -4,6 +4,7 @@ class_name HurtboxComponent
 
 
 signal hurtbox_hit(body : CollisionObject2D)
+signal hurtbox_exited(body : CollisionObject2D)
 
 #Variables
 @export var hurt_damage : int
@@ -12,5 +13,10 @@ signal hurtbox_hit(body : CollisionObject2D)
 
 
 #---------------------------------------------------------------------------------------------------------------------------
-func _on_body_entered(body: CollisionObject2D):
+func _on_body_entered(body):
 	hurtbox_hit.emit(body)
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+func _on_body_exited(body):
+	hurtbox_exited.emit(body)
--- game_level_3/scripts/main/bullet.gd
+++ game_level_3/scripts/main/bullet.gd
@@ -3,56 +3,67 @@ extends CharacterBody2D
 class_name Bullet
 
 
+@export_group("Bullet Nodes")
 @export var hurtbox : HurtboxComponent
-@export var collision : CollisionShape2D
+@export var death_timer_node : Timer
+@export var collision_timer_node : Timer
 
+
+@export_group("Bullet Stats")
 @export var speed : float = 600.0
 @export var damage : int = 5
 @export var max_pierce : int = 1
+@export var life_time : float = 1.0
+@export var collision_time : float = 0.15
 
 var current_pierce_count := 0
 var fake_velocity := Vector2.ZERO
-var has_collided : bool = false
 var collide_array : Array = []
+var collision_hit : bool = false
 
 
 #---------------------------------------------------------------------------------------------------------------------------
 func _ready():
 	collide_array.clear()
-	current_pierce_count = 0
+	current_pierce_count = 1
+	collision_hit = false
 	
-	has_collided = false
-	collision.disabled = false
+	death_timer_node.start(life_time)
 	
 	if hurtbox:
 		hurtbox.hurtbox_hit.connect(on_enemy_hit)
+		hurtbox.hurtbox_exited.connect(enemy_un_hit)
 
 
 #---------------------------------------------------------------------------------------------------------------------------
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
 	var dir = Vector2.RIGHT.rotated(rotation)
 	
 	fake_velocity = dir * speed
 	velocity = Vector2(fake_velocity.x, fake_velocity.y / 2) #Make velocity isometric
 	
-	if has_collided == false:
-		move_and_collide(velocity * delta)
-
-	else:
-		move_and_slide()
+	move_and_slide()
 
 
 #---------------------------------------------------------------------------------------------------------------------------
 func on_enemy_hit(body) -> void:
-	pass
+	if collide_array.find(body) == -1: 
+		collide_array.append(body)
+		_on_collision_timer_timeout()
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+func enemy_un_hit(body) -> void:
+	collide_array.erase(body)
 
 
 #---------------------------------------------------------------------------------------------------------------------------
 func actually_hit() -> void:
 	if current_pierce_count >= max_pierce:
 		delete_bullet()
-		
+	
 	current_pierce_count += 1
 
 
@@ -63,13 +74,25 @@ func implement_stats():
 
 #---------------------------------------------------------------------------------------------------------------------------
 func delete_bullet():
-	has_collided = true
-	collision.disabled = true
-	speed *= -1
+	speed = 0
 	
 	var delete_tween = create_tween()
-	delete_tween.tween_property(self, "speed", 0, 0.35).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT).from_current()
+	delete_tween.tween_property(self, "scale", Vector2(0, 0), 0.35).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT).from_current()
 	
 	await get_tree().create_timer(0.35, false).timeout
 	
 	queue_free()
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+#TImer Nodes
+func _on_delete_timer_timeout():
+	delete_bullet()
+
+
+func _on_collision_timer_timeout():
+	collision_hit = false
+	if collide_array.is_empty() == false and collision_hit == false:
+		actually_hit()
+		collision_timer_node.start(collision_time)
+		collision_hit = true
