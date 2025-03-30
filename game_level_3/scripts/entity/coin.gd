0488e021e821a9c06685ea1780824d00b5393b22
Roryrex <rorywildy@gmail.com>
2025-03-30 22:35:11 +1300

    Minor changes
    Fixed speed clamp on coins.
    Fixed player UI positioning on screen.

--- game_level_3/scripts/entity/coin.gd
+++ game_level_3/scripts/entity/coin.gd
@@ -9,8 +9,8 @@ var velocity = Vector2.ZERO
 func _process(delta):
 	if target:
 		speed += 450 * delta
-		clamp(speed, 0, 800)
-		
+		speed = clamp(speed, 0, 600)
+			
 		var dir = get_angle_to(Global.player_position)
 		velocity = Vector2.RIGHT.rotated(dir) * speed
 		global_position += velocity * delta
@@ -21,6 +21,7 @@ func _process(delta):
 func _on_collection_radius_body_entered(_body : Player):
 	Global.player_coins += 1
 	queue_free()
+
 
 #---------------------------------------------------------------------------------------------------------------------------
 func _on_body_entered(body : Player):
