8b327885a0df75e47a4a266632b473106ba44ff0
Roryrex <rorywildy@gmail.com>
2025-07-11 20:09:09 +1200

    More Tiles
    Added more tiles to tileset.
    Changes to onscreen detectors for optimization and less glitches.

--- game_level_3/scripts/entity/particles/visibilty_script.gd
+++ game_level_3/scripts/entity/particles/visibilty_script.gd
@@ -5,10 +5,10 @@ extends VisibleOnScreenNotifier2D
 func _ready():
 	self.connect("screen_exited", _on_screen_exited)
 	
-
 	if call_deferred("is_on_screen") == false:
-		get_parent().queue_free()
+		_on_screen_exited()
 
 #---------------------------------------------------------------------------------------------------------------------------
 func _on_screen_exited():
+	get_parent().visible = false
 	get_parent().queue_free()
