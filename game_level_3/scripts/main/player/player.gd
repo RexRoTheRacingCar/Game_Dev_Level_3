5f2d37c6d53497488262f490d9223646533fcbfa
Roryrex <rorywildy@gmail.com>
2025-07-17 13:17:24 +1200

    WIP Room
    Added a new WIP fight room.

--- game_level_3/scripts/main/player/player.gd
+++ game_level_3/scripts/main/player/player.gd
@@ -77,7 +77,7 @@ func _ready():
 	p_knockback_taken = Vector2.ZERO
 	velocity = Vector2.ZERO
 	
-	Global.player_rerolls = 100
+	Global.player_rerolls = 3
 	
 	$PlaceholderSprite2D.self_modulate = Color("ffffff")
 	
